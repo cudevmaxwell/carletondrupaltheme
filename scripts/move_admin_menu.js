289668550436db0c7b1d5a1ed17f9e9a17c27905
Kevin Bowrin <kevin.bowrin@carleton.ca>
2014-11-28 15:07:06 -0500

    Added svgeezy init and move_admin to move the admin bar to the top of the page

---
+++ scripts/move_admin_menu.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($){
+  playNiceAdmin();
+});
+
+function playNiceAdmin(){
+  jQuery(document).ready(function($){
+  if($("#admin-menu").length){
+    //Get admin menu to play nice
+    $("#admin-menu").detach().prependTo('#page');
+  }
+  else{
+    window.setTimeout(playNiceAdmin,20)
+  }
+  });
+}
